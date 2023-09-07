c397be13f315531a76eceb5676ad966b5e33586b
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-07 10:33:18 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main function 
+ * Return: 1 always return 1 when successsful
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
