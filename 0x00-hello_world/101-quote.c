3e6109c2b7789be3cb3268a99e60cdce8405da70
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-07 14:07:39 -0700

    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - main function
- * Return 1 when successfull
+ * Return: 1 when successfull
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
