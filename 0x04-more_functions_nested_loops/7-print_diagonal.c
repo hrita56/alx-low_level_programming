3f9715991f25db3cd35d8e86de3fd6406050bcc2
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-14 07:27:42 -0700

    ./7-diagonals | cat -e

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - function to print diagonal line
+ * @n: number of time to be printed
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j <= i ; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
