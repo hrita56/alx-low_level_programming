64d9b5861f106d5c995d2412d2670abc58eb29e6
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-18 05:32:27 -0700

    ./1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swapp two intergers
+ * @a: value 1
+ * @b: value 2
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+
+}
