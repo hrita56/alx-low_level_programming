2aff2fbc20fd0afa4c35fdb298695e72143d2fdc
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-18 04:30:24 -0700

    ./0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function to rest n to 98
+ * @n: pointrr to the value to be chnaged
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A readme file for 0x05. C - Pointers, arrays and strings project
