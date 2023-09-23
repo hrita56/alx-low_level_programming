caa4255a220b3d9ccf9c37e6d91ab6d59b718608
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-22 18:12:36 -0700

    -strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _strcmp - compare string values
- * @s1: parametter 1
- * @s2: parameter 2
+ * _strcmp - function to compare string values
+ * @s1: parameter 1
+ * @s2: parametr 2
  * Return: int
  */
 int _strcmp(char *s1, char *s2)
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] != s2[2])
+		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
