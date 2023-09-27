56ffeaac095073a13b2bf780e1620150253bb1e3
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-27 03:10:14 -0700

    ./5-sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+int actual_sqrt_recursion(int n, int i);
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: number to caluculate the saquare root
+ * Return: the square rot
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+/**
+ * actual_sqrt_recursion - fucntion to find natural square root of number
+ * @n: number to calcuates square root
+ * @i: iteratiion
+ * Return: square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (actual_sqrt_recursion(n, i + 1));
+}
