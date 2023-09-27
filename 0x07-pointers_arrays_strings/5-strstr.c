90bfb8dd47749c4bde0127ac1a41e344f2935b73
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-26 21:04:54 -0700

    ./5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,2 +1,24 @@
 #include "main.h"
+/**
+ * strctrs - function to locates a substring
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0;
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0' ; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
 
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
