a292aad3abc4d845811e4636376c13ff2ec9dacc
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-26 20:23:49 -0700

    ./100-set_string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,3 +1,9 @@
+#include "main.h"
+/**
+ * set_string - function o set values of pointer to achar
+ * @s: pointer
+ * @to: pointer char
+ */
 void set_string(char **s, char *to)
 {
 	*s = to;
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
+void set_string(char **s, char *to);
 #endif
