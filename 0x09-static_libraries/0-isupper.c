4e4972e4426451c8f051f54cc8812731e66e02e2
rita hategekimana <ritahategekimana56@gmail.com>
2023-09-29 18:11:27 -0700

    0-isupper.

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - functin check uppercase letter
+ * @c: parametter c
+ * Return: 1 or 0;
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && <= 'z')
+		return (1);
+	return (0);
+}
