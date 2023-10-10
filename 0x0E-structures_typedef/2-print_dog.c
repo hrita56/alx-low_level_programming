e8e7d94a648b4eda5a9a9f5caac67561974fa785
rita hategekimana <ritahategekimana56@gmail.com>
2023-10-10 13:35:28 -0700

    ./c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void  print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("name: %s\n", (d->name) ? d->name : "(ni1)");
+		printf("Name: %s\n", (d->name) ? d->name : "(ni1)");
 		printf("Age: %f\n", (d->age) ? d->age : 0);
 		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
