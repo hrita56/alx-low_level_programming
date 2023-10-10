12c90a13d357f616ceb38a0fdd31b1aa22666a68
rita hategekimana <ritahategekimana56@gmail.com>
2023-10-10 13:00:29 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ *  * init_dog - dog structure
+ *   * @name: dog name
+ *    * @age: dog age
+ *     * @owner: dog owener
+ *     * @d: pointer to dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
